1a86c3e7868c4c590743e72810b6ad93832c3787
Chris Roberts <code@chrisroberts.org>
2016-07-28 10:02:35 -0700

    Use concrete source class not abstract for spec

--- spec/omnivore_message_spec.cr
+++ spec/omnivore_message_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Omnivore::Message do
 
   it "should create a new message and set identifier" do
-    source = Omnivore::Source.new("test", Channel(Omnivore::Message).new(1))
+    source = Omnivore::Source::Internal.new("test", Channel(Omnivore::Message).new(1))
     message = Omnivore::Message.new(source)
     message.identifier.should be_a(String)
     message.identifier.size.should be > 0
@@ -11,21 +11,21 @@ describe Omnivore::Message do
 
   it "should create a message using given data" do
     data = {"id" => "fubar"} of String => JSON::Type
-    source = Omnivore::Source.new("test", Channel(Omnivore::Message).new(1))
+    source = Omnivore::Source::Internal.new("test", Channel(Omnivore::Message).new(1))
     message = Omnivore::Message.new(data, source)
     message.identifier.should eq("fubar")
   end
 
   it "should fetch data via hashy helpers" do
     data = {"id" => "fubar", "data" => "value"} of String => JSON::Type
-    source = Omnivore::Source.new("test", Channel(Omnivore::Message).new(1))
+    source = Omnivore::Source::Internal.new("test", Channel(Omnivore::Message).new(1))
     message = Omnivore::Message.new(data, source)
     message.get(:data, type: :string).should eq("value")
   end
 
   it "should set data via hashy helpers" do
     data = {"id" => "fubar"} of String => JSON::Type
-    source = Omnivore::Source.new("test", Channel(Omnivore::Message).new(1))
+    source = Omnivore::Source::Internal.new("test", Channel(Omnivore::Message).new(1))
     message = Omnivore::Message.new(data, source)
     message.set(:data, :content, value: "testing")
     message.get(:data, :content, type: :string).should eq("testing")
@@ -33,7 +33,7 @@ describe Omnivore::Message do
 
   it "should mark itself confirmed after being confirmed" do
     data = {"id" => "fubar"} of String => JSON::Type
-    source = Omnivore::Source.new("test", Channel(Omnivore::Message).new(1))
+    source = Omnivore::Source::Internal.new("test", Channel(Omnivore::Message).new(1))
     message = Omnivore::Message.new(data, source)
     message.confirmed?.should be_false
     message.confirm
